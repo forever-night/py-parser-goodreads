1bfa53cd8dff96fd6456d94dd8d97d7bc54c1d7a
Anna <anna.upintheair@gmail.com>
2014-11-01 22:25:18 +0100

    refactoring

--- gdr-parser.py
+++ gdr-parser.py
@@ -5,6 +5,7 @@ shelf_computers = 'http://www.goodreads.com/review/list/20186983-anna?shelf=comp
 to_read = parse(shelf_to_read).getroot()
 computers = parse(shelf_computers).getroot()
 
+
 # get number of webpages pages to iterate
 def get_num_webpages(root):
     x_num_books = root.xpath('//div[@id="header"]//span[@class="greyText"]')
@@ -19,10 +20,12 @@ def get_num_webpages(root):
 webpages_toread = get_num_webpages(to_read)
 webpages_comp = get_num_webpages(computers)
 
+
 # for debugging
 def printelems(_list):
     for elem in _list:
         print elem
+        
 
 # trim whitespace & \n
 def append_text(x_list, _list):
@@ -30,6 +33,7 @@ def append_text(x_list, _list):
         elem_text = elem.text.strip()
         _list.append(elem_text)
         elem.clear()
+        
 
 # from to-read
 titles = []
@@ -45,6 +49,7 @@ for n in range(1, webpages_toread + 1):
     append_text(x_titles, titles)
     x_auth = to_read.xpath('//td[@class="field author"]//a[text()]')
     append_text(x_auth, authors)
+    
 
 # from 'computers'
 comp_titles = []
@@ -56,6 +61,7 @@ for n in range(1, webpages_comp + 1):
 
     x_comp_titles = computers.xpath('//td[@class="field title"]//a[text()]')
     append_text(x_comp_titles, comp_titles)
+    
 
 # compare to-read and computers
 toread_comp_titles = []
@@ -65,24 +71,29 @@ for elem in comp_titles:
             toread_comp_titles.append(elem)
         else:
             pass
+        
 
 # mapping keys from list_0 to values from list_1
 def add_to_dict(list_0, list_1, dictionary):
     if len(list_0) == len(list_1):
         for n in range(0, len(list_0)):
             dictionary[list_0[n]] = list_1[n]
+            
 
 # dictionary for to-read
 title_author = dict()
 add_to_dict(titles, authors, title_author)
 
+
 # map keys in 'computer' dictionary to values in 'toread'
 def map_toread_comp(comp_dict, toread_dict, toread_comp_keys):
     for n in range(0, len(toread_comp_keys)):
         comp_dict[toread_comp_keys[n]] = toread_dict[toread_comp_keys[n]]
+        
 
 # dictionary for computers
 comp_title_author = dict()
 map_toread_comp(comp_title_author, title_author, toread_comp_titles)
+
 
 printelems(comp_titles)
